9e3a85f5ca6fd4418320bf7ad16e6a64fd00fcf3
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-07-20 23:56:31 -0500

    update

--- src/api.js
+++ src/api.js
@@ -3,4 +3,18 @@ module.exports = {
     return fetch('/api/movies')
       .then(response => response.json())
   },
+
+  addMovie: () =>{
+    const movie = {title: title, rating: rating};
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movie),
+    };
+   return fetch('/api/movies', options)
+
+  }
+
 };
--- src/index.js
+++ src/index.js
@@ -7,30 +7,36 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
-const $ = require('jquery');
+const {getMovies,addMovie} = require('./api.js');
+const $ = require("jquery");
 
-getMovies().then((movies) => {
-  $('#moreStuff').html(`<div>Here are all the movies: </div>`);
-  $('#movieStuff').html('');
-  let movie = '';
-  movies.forEach(({title, rating, id}) => {
-    movie += `<div>${title} - rating: ${rating}</div>`;
+
+function loaded() {
+  getMovies().then((movies) => {
+    $('#moreStuff').html(`<div>Here are all the movies: </div>`);
+    $('#movieStuff').html('');
+    let movie = '';
+    movies.forEach(({title, rating, id}) => {
+      movie += `<div>${title} - rating: ${rating}</div>`;
+    });
+    $(movie).appendTo('#movieStuff')
+  }).catch((error) => {
+    console.log(error);
   });
-  $(movie).appendTo('#movieStuff')
-}).catch((error) => {
-  console.log(error);
-});
+}
+loaded();
 
-// var tbody = document.querySelector('#movies');
-// var submitButton = document.querySelector('#submit');
-//
-// var myInput = document.querySelector("#myInput");
-//
-// var userInput = document.querySelector('#userInput');
-// var userMovie = document.querySelector('#userSubmit');
-//
-// tbody.innerHTML = renderMovies(movies);
 
-// submitButton.addEventListener('click', updateMovies);
-// userMovie.addEventListener('click', inputMovie);
+
+ $('#userSubmit').click(()=> {
+   let typeMovieTitle= $('#title').val();
+   let typeMovieRating= $('#rating').val();
+   addMovie(typeMovieTitle, typeMovieRating).then((response)=>{
+     loaded()
+  });
+  console.log("movie add")
+  }).catch(()=> {
+   console.log("error")
+
+ });
+
