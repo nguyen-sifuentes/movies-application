7422c84135ef2cdd3561d935c45ac1cc15c67263
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-07-22 15:17:32 -0500

    more stuff

--- src/index.js
+++ src/index.js
@@ -10,28 +10,22 @@ sayHello('World');
 const {getMovies,addMovie,deleteMovie} = require('./api.js');
 const $ = require("jquery");
 
-
 $('#deleteSubmit').on('click', function (e) {
-    e.preventDefault()
+    e.preventDefault();
     console.log(e);
     console.log('clicking delete button');
     let typeMovieId = $('#deleteMoviesSelect').find(":selected").val();
     console.log(typeMovieId);
     deleteMovie(typeMovieId);
     loaded();
-
 });
-
-
-
-
 function loaded() {
     $('#moreStuff').html(`<div>Here are all the movies: </div>`);
   getMovies().then((movies) => {
     $('#movieStuff').html('');
     let movie = '';
     movies.forEach(({title, rating, id}) => {
-      movie += `<div>${title} - rating: ${rating}</div>`;
+      movie += `<div class="text-justify">${title}</div><div class="text-justify">${rating} stars</div>`;
     });
     $(movie).appendTo('#movieStuff');
 
@@ -43,18 +37,11 @@ function loaded() {
       // console.log(movieDelete);
       $('#deleteMoviesSelect').html(movieDelete).then($("#deleteSubmit").removeAttr("disabled"))
 
-
-
-
-
   }).catch((error) => {
     console.log(error);
   });
 }
 loaded();
-
-
-
  $('#userSubmit').click(()=> {
    let typeMovieTitle= $('#title').val();
    let typeMovieRating= $('#rating').val();
@@ -66,74 +53,3 @@ loaded();
    console.log("error")
 
  });
-
-// var e = document.getElementById("ddlViewBy");
-//     let strUser = typeMovieId.options[typeMovieId.selectedIndex].value;
-// $('#deleteSubmit').click((e)=> {
-//     console.log(e);
-//     e.preventDefault();
-//     console.log('clicking delete button');
-//     let typeMovieId = $('#deleteMoviesSelect').find(":selected").val();
-//     console.log(typeMovieId);
-//     // let typeMovieRating= $('#rating').val();
-//     // deleteMovie(typeMovieId).then((response)=>{
-//     //     loaded()
-//     // });
-//     // console.log("movie add")
-// }).catch(()=> {
-//     console.log("error")
-
-// });
-// let deleteMovieData;
-
-// const deleteMovies = () =>
-//         console.log('deleteMoviesFunction');
-    // getMovies().then((movies) => {
-        // console.log('Here are all the movies:');
-    //     $('#deleteMoviesSelect').html ("");
-    //     let movieDelete = "<option>pick a movie to delete</option>";
-    //     movies.forEach(({title, rating, id}) => {
-    //         movieDelete +=`<option value="${title}">${title}</option>`;
-    //     });
-    //     console.log(movieDelete);
-    //     $('#deleteMoviesSelect').html(movieDelete).then($("#deleteSubmit").removeAttr("disabled"))
-    // })
-    //gets value for movies option tag
-    //     .then($("select#deleteMovies").change(function(){
-    //         deleteMovieData = $(this).children("option:selected").val();
-    //         console.log(deleteMovieData)
-    //     }))
-    //     .catch((error) => {
-    //         console.log(error);
-    //     });
-
-
-//Deletes a movie on submit
-// $('#deleteSubmit').click(function (e) {
-//     e.preventDefault();
-//     getMovies().then((movieData) => {
-//         movieData.forEach(({title, rating, id}) => {
-//             if(deleteMovieData===title){
-//                 const userMovies = {
-//                     title: title,
-//                     rating: rating
-//                 };
-//                 const url = '/api/movies/'+id;
-//                 const options = {
-//                     method: 'DELETE',
-//                     headers: {
-//                         'Content-Type': 'application/json'
-//                     },
-//                     body: JSON.stringify(userMovies)
-//                 };
-//                 fetch(url, options)
-//                     .then(movies)
-//                     .then(editMovies)
-//                     .then(deleteMovies);
-//             }
-//         });
-//     });
-// });
-//
-// // deleteMovies();
-
